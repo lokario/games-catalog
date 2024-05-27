fd8444af697ec8dc02626d4d6438bb351c78dadf
Juan Diaz <32882631+lokario@users.noreply.github.com>
2024-05-26 21:39:29 -0400

    Add critics score

--- src/Components/GameCard/GameCard.tsx
+++ src/Components/GameCard/GameCard.tsx
@@ -1,8 +1,9 @@
 import { Game } from "../../hooks/useGames";
-import { Card, CardBody, Heading, Image, Text, VStack } from "@chakra-ui/react";
+import { Card, CardBody, HStack, Heading, Image, Text, VStack } from "@chakra-ui/react";
 import getCroppedImageUrl from "../../services/image-url";
 import PlatformsList from "../PlatformsList";
 import Rating from "../Rating";
+import CriticScore from "../CriticScore/CriticScore";
 
 function GameCard({ game }: { game: Game }) {
 	return (
@@ -13,8 +14,14 @@ function GameCard({ game }: { game: Game }) {
 			<Image src={getCroppedImageUrl(game.background_image)} />
 			<CardBody>
 				<VStack>
-					<PlatformsList platforms={game.parent_platforms.map(p => p.platform)} />
+					<HStack
+						width="100%"
+						justifyContent="space-between">
+						<PlatformsList platforms={game.parent_platforms.map(p => p.platform)} />
+						<CriticScore score={game.metacritic} />
+					</HStack>
 					<Heading
+						width="100%"
 						cursor="pointer"
 						fontSize="2xl">
 						<Text
